40f390568c7f671471380119874f68eea1c27290
Pa-ppy <rayjacks07@gmail.com>
2025-02-10 15:21:10 +0000

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches and exports the TODO list of an employee to a CSV file."""
+"""Fetches and exports an employee's TODO list to a CSV file."""
 import csv
 import requests
 import sys
@@ -15,13 +15,15 @@ if __name__ == "__main__":
     user_data = requests.get(user_url).json()
     username = user_data.get("username")
 
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?\
-            userId={employee_id}"
+    todos_url = (
+            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+            )
     todos_data = requests.get(todos_url).json()
 
     filename = f"{employee_id}.csv"
     with open(filename, mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todos_data:
-            writer.writerow([employee_id, username,
-                             task.get("completed"), task.get("title")])
+            writer.writerow([
+                employee_id, username, task.get("completed"), task.get("title")
+            ])
