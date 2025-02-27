01efbadbfdbc9dc7d1e929a2c296601864affd54
Pa-ppy <rayjacks07@gmail.com>
2025-02-27 03:10:19 +0000

    Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,47 +1,38 @@
-# Puppet manifest to fix Apache 500 error by ensuring required modules and permissions are set
+# This Puppet manifest fixes Apache's 500 error by ensuring necessary permissions and missing modules are configured.
 
-class fix_apache_500 {
-
-    # Ensure Apache is installed
+class apache_fix {
+    # Ensure Apache is installed and enabled
     package { 'apache2':
         ensure => installed,
     }
 
-    # Ensure PHP module is installed (required for WordPress)
-    package { 'libapache2-mod-php5':
-        ensure => installed,
+    service { 'apache2':
+        ensure    => running,
+        enable    => true,
+        require   => Package['apache2'],
     }
 
-    # Ensure the correct permissions for /var/www/html
+    # Fix missing or incorrect permissions for /var/www/html
     file { '/var/www/html':
         ensure  => directory,
         owner   => 'www-data',
         group   => 'www-data',
         mode    => '0755',
-        recurse => true,
     }
 
-    # Ensure Apache service is running and enabled
-    service { 'apache2':
-        ensure     => running,
-        enable     => true,
-        require    => Package['apache2'],
-    }
-
-    # Ensure Apache modules are enabled
-    exec { 'enable-apache-php':
-        command => '/usr/sbin/a2enmod php5',
-        unless  => '/bin/ls /etc/apache2/mods-enabled/ | /bin/grep php5',
+    # Ensure the required PHP module is enabled
+    exec { 'enable_php_module':
+        command => '/usr/sbin/a2enmod php7.0',
+        unless  => '/usr/sbin/a2query -m php7.0',
         notify  => Service['apache2'],
     }
 
     # Restart Apache to apply changes
-    exec { 'restart-apache':
-        command     => '/usr/sbin/service apache2 restart',
+    exec { 'restart_apache':
+        command => '/etc/init.d/apache2 restart',
         refreshonly => true,
-        subscribe   => [Package['libapache2-mod-php5'], Exec['enable-apache-php']],
+        subscribe   => Exec['enable_php_module'],
     }
 }
 
-# Apply the class
-include fix_apache_500
+include apache_fix
