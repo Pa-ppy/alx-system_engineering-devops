51f0203432008db6aa556bc05c53fc4ff522e769
Pa-ppy <rayjacks07@gmail.com>
2025-02-27 03:20:56 +0000

    Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixes Apache 500 error by correcting a typo in wp-settings.php if the file exists
 exec { 'fix_wp_settings':
-  command => '/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  onlyif  => '/bin/test -f /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => shell,
+  onlyif   => 'test -f /var/www/html/wp-settings.php',
 }
