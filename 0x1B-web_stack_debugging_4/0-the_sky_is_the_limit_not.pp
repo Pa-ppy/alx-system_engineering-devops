03122e2076e4c7e4a15e8dad686ca13232aa85ed
Pa-ppy <rayjacks07@gmail.com>
2025-03-07 15:03:01 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,29 +1,19 @@
-# This Puppet manifest fixes the issue of high failed requests by adjusting Nginx configuration and increasing file descriptor limits.
+# This Puppet manifest increases the file descriptor limit for Nginx
 
-# Increase the file descriptor limit for Nginx
 exec { 'increase-nginx-ulimit':
   provider => shell,
   command  => 'sed -i "s/ULIMIT=\"-n [0-9]*\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   unless   => 'grep -q "ULIMIT=\"-n 4096\"" /etc/default/nginx',
 }
 
-# Restart Nginx to apply the new ULIMIT setting
-exec { 'restart-nginx':
+exec { 'stop-nginx':
   provider => shell,
-  command  => 'service nginx restart',
+  command  => 'service nginx stop',
   require  => Exec['increase-nginx-ulimit'],
 }
 
-# Increase system-wide file descriptor limits
-exec { 'increase-system-ulimit':
+exec { 'start-nginx':
   provider => shell,
-  command  => 'echo "* soft nofile 65536" >> /etc/security/limits.conf && echo "* hard nofile 65536" >> /etc/security/limits.conf',
-  unless   => 'grep -q "* soft nofile 65536" /etc/security/limits.conf',
-}
-
-# Reload PAM limits to apply system-wide changes
-exec { 'reload-pam-limits':
-  provider => shell,
-  command  => 'sysctl -p',
-  require  => Exec['increase-system-ulimit'],
+  command  => 'service nginx start',
+  require  => Exec['stop-nginx'],
 }
